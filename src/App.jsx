444dc88bdabd9f279c365a6af1d6af853f5b149b
jefersonprimer <jefersonpriimer@gmail.com>
2024-07-05 11:04:55 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState, useRef } from 'react';
+import './App.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import brasCubasImg from './assets/bras_cubas.jpeg';
+import Capa from './Capa';
+import SeletorCapitulos from './SeletorCapitulos';
+import BotoesControle from './BotoesControle';
+import livro from './assets/capitulos/livro';
+import GerenciadorFaixa from './GerenciadorFaixa';
+
+function App() {
+  // let taTocando = false;
+  const [taTocando, definirTaTocando] = useState(false);
+  const [faixaAtual, definirFaixaAtual] = useState(0);
+  const tagAudio = useRef(null);
+
+  const informacoesLivro = {
+    nome: 'Memórias Póstumas de Brás Cubas',
+    autor: 'Machado de Assis',
+    totalCapitulos: 2,
+    capa: brasCubasImg,
+    capitulos: livro,
+    textoAlternativo: 'Capa do livro Memórias Póstumas de Brás Cubas.',
+  };
+
+  const tocarFaixa = () => {
+    tagAudio.current.play();
+    definirTaTocando(true);
+  };
+
+  const pausarFaixa = () => {
+    tagAudio.current.pause();
+    definirTaTocando(false);
+  };
+
+  const tocarOuPausarFaixa = () => {
+    if (taTocando) {
+      pausarFaixa();
+    } else {
+      tocarFaixa();
+    }
+  };
+
+  return (
+    <>
+      <Capa
+        imagemCapa={informacoesLivro.capa}
+        textoAlternativo={informacoesLivro.textoAlternativo}
+      />
+      <SeletorCapitulos capituloAtual={faixaAtual + 1} />
+      <GerenciadorFaixa
+        faixa={informacoesLivro.capitulos[faixaAtual]}
+        referencia={tagAudio}
+      />
+      <BotoesControle
+        taTocando={taTocando}
+        tocarOuPausarFaixa={tocarOuPausarFaixa}
+      />
+    </>
+  );
+}
+
+export default App;
